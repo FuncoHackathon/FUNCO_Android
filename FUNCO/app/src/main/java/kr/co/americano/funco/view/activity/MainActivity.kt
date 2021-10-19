d0c08cc206a26dd78e7b9d2cc2c49db567827745
snack655 <80940200+snack655@users.noreply.github.com>
2021-10-19 12:35:23 +0900

    Add : MVVM Package structures
    2021-10-19

--- FUNCO/app/src/main/java/kr/co/americano/funco/view/activity/MainActivity.kt
+++ FUNCO/app/src/main/java/kr/co/americano/funco/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package kr.co.americano.funco
+package kr.co.americano.funco.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.americano.funco.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
