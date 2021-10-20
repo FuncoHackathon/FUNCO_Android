b31f243b15455bb2fa80932e489be953dd05f42d
snack655 <80940200+snack655@users.noreply.github.com>
2021-10-20 18:39:00 +0900

    Add : Register View
    2021-10-20

---
+++ FUNCO/app/src/main/java/kr/co/americano/funco/view/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.americano.funco.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.americano.funco.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
